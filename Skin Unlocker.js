db17a167e98411dad65fad50a4b2d38afab73772
3lectr0N!nj@ <aaronjs099@gmail.com>
2026-06-18 16:36:50 +0300

    Update Skin Unlocker.js

--- Skin Unlocker.js
+++ Skin Unlocker.js
@@ -1,15 +1,3 @@
-// ==UserScript==
-// @name         DashCraft.io Skin Unlocker
-// @author       3lectr0N!nj@
-// @namespace    https://github.com/3lectr0Ninja
-// @version      1
-// @description  Unlock all Skins
-// @match        https://dashcraft.io/*
-// @icon         https://dashcraft.io/assets/icons/favicon-32x32.png
-// @grant        none
-// @run-at       document-start
-// ==/UserScript==
-
 (function () {
     'use strict';
     const originalFetch = window.fetch;
@@ -19,7 +7,7 @@
         if(response.url=="https://api.dashcraft.io/auth/account"){
             const clone = await response.clone()
             const data = await response.clone().json();
-            let exp = 1000000
+            let exp = 10000000000
             data.xp=exp
             data.leagueNr=exp
             data.levelData.isMaxLevel=true
